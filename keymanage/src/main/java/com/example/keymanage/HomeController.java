82f2f3db18cc41b4dd1cdb3725ca931a2c99f3bc
ycab <1210626681@qq.com>
2018-08-13 09:59:27 +0800

    项目搭建

---
+++ keymanage/src/main/java/com/example/keymanage/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.keymanage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description: $
+ * @Param: $
+ * @return: $
+ * @Author: your name
+ * @date: 2018.7.30$
+ */
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    @GetMapping(value = "/index")
+    public String say()
+    {
+        return "index.html";
+    }
+}
---
+++ keymanage/src/main/java/com/example/keymanage/KeymanageApplication.java
@@ -0,0 +1,12 @@
+package com.example.keymanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeymanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeymanageApplication.class, args);
+	}
+}
